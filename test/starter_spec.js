3c91dc71bb38a8aea793b25c567a658ca9ac2ced
Jeffrey Linwood <jlinwood@gmail.com>
2017-06-02 12:06:12 -0500

    Add Docker Compose for integration testing SDK Starter Node web app with Frisby V2

---
+++ test/starter_spec.js
@@ -0,0 +1,51 @@
+const frisby = require('frisby');
+
+frisby.baseUrl('http://sdk-starter-node:3000')
+
+describe('SDK Starter Kit Test Suite', function () {
+  it('should retrieve a token', function (done) {
+    frisby.get('/token')
+      .expect('status', 200)
+      .done(done)
+  })
+  it('should retrieve the configuration check', function (done) {
+    frisby.get('/config')
+      .expect('status', 200)
+      .done(done)
+  })
+  it('should be able to create a binding', function (done) {
+    frisby.post('/register')
+      .expect('status', 200)
+      .done(done)
+  })
+  it('should be able to send a notification', function (done) {
+    frisby.post('/send-notification')
+      .expect('status', 200)
+      .done(done)
+  })
+  it('should retrieve home page', function (done) {
+    frisby.get('/')
+      .expect('status', 200)
+      .done(done)
+  })
+  it('should retrieve Sync page', function (done) {
+    frisby.get('/sync')
+      .expect('status', 200)
+      .done(done)
+  })
+  it('should retrieve Chat page', function (done) {
+    frisby.get('/chat')
+      .expect('status', 200)
+      .done(done)
+  })
+  it('should retrieve Video page', function (done) {
+    frisby.get('/video')
+      .expect('status', 200)
+      .done(done)
+  })
+  it('should retrieve Notify page', function (done) {
+    frisby.get('/notify')
+      .expect('status', 200)
+      .done(done)
+  })
+})
